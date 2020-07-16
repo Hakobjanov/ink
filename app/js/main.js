72bbffda746aa017012faeb688212cfd125ba48b
vahan akopjanian <vahan.bclub@gmail.com>
2020-07-16 23:55:12 +0300

    added fonts including "GT America" and circle cursor

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
 $(function () {});
+//cursor animations
+const mouse = document.querySelector(".cursor");
+
+function cursor(e) {
+  //   console.log(wheel);
+  mouse.style.top = e.y + "px";
+  mouse.style.left = e.x + "px";
+}
+
+//event listeners
+window.addEventListener("mousemove", cursor);
