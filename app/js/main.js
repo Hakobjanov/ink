768f6e42c5ebb92d485f231e41e281da32d56e72
vahan akopjanian <vahan.bclub@gmail.com>
2020-07-18 13:00:44 +0300

    added swiper and bg-line animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,51 @@
-$(function () {});
+//swiper
+const swiperContainer = document.querySelector(".swiper-container");
+const cursorText = document.querySelector(".cursor-text");
+const blueLine = document.querySelector(".blue-line");
+const photoFrameWrapper = document.querySelector(".photo-frame-wrapper");
+const photoFrames = document.querySelectorAll(".photo-frame");
+
+blueLine.addEventListener("animationend", () => {
+  photoFrames[1].classList.remove("transparent");
+  setTimeout(() => {
+    photoFrames[2].classList.remove("transparent");
+  }, 250);
+  setTimeout(() => {
+    photoFrames[0].classList.remove("transparent");
+  }, 500);
+});
+
+let mySwiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 3,
+});
+
+let lastSlideIndex = 0;
+
+mySwiper.on("slideChange", () => {
+  if (
+    mySwiper.activeIndex === lastSlideIndex - 1 ||
+    mySwiper.activeIndex - lastSlideIndex > 1
+  ) {
+    photoRotator.goLeft();
+  } else {
+    photoRotator.goRight();
+  }
+  lastSlideIndex = mySwiper.activeIndex;
+});
+
+//const scene = document.querySelector(".frame-1");
 
 const photoRotator = new Rotator(".photo-frame", [
   "/img/image0.png",
   "/img/image1.png",
   "/img/image2.png",
 ]);
+
+//parallax js
+let parallaxInstance = new Parallax(photoFrameWrapper, {
+  relativeInput: true,
+});
 
 //cursor animations
 const mouse = document.querySelector(".cursor");
@@ -15,5 +56,21 @@ function cursor(e) {
   mouse.style.left = e.x + "px";
 }
 
+function activeCursor(e) {
+  const item = e.target;
+  if (item.classList.contains("menu-link")) {
+    console.log(item.classList);
+    mouse.classList.add("mouse-active");
+  } else {
+    mouse.classList.remove("mouse-active");
+  }
+  if (e.path.includes(swiperContainer)) {
+    cursorText.classList.add("cursor-drag");
+  } else {
+    cursorText.classList.remove("cursor-drag");
+  }
+}
+
 //event listeners
 window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
